93b74adb2f098a95d88931f1cd7d0ab32ee265a2
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-08-09 03:22:24 +0000

    ci: eliminados steps innecesarios para el deploy

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -16,7 +16,7 @@ func main() {
 
 	http.HandleFunc("/health", handlers.HealthHandler)
 
-	fmt.Printf("🚀 Servidor corriendo en puerto %v\n", config.Port)
+	fmt.Printf("🚀 Servidor ejecutándose en puerto %v\n", config.Port)
 
 	address := fmt.Sprintf(":%v", config.Port)
 	log.Fatal(http.ListenAndServe(address, nil))

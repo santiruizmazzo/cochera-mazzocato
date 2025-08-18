e401d89da281f76bd44d8a1747e71f61977b8228
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-08-18 18:21:18 -0300

    build: creado archivo de configuracion de ESLint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module"
+    },
+    rules: {},
+  },
+  {
+    ignores: ["node_modules/"],
+  },
+];

de722b8688cbc316236a2dd8eb75ebdc70096e32
Santiago Ruiz Mazzocato <santiago@email.com>
2025-08-18 00:23:26 +0000

    feat: creado header que contiene nombre y version actual de la app

---
+++ frontend/app.js
@@ -0,0 +1,17 @@
+async function obtenerVersion() {
+    try {
+        const response = await fetch('http://localhost:5000/health');
+        if (!response.ok) {
+            return "☹"
+        }
+        return await response.json().then((data) => {
+            return "v" + data['version']
+        })
+    } catch (error) {
+        console.error('Hubo un problema con el fetch:', error);
+        return "☹"
+    }
+}
+
+var cajaDeVersion = document.querySelector('.caja-version')
+cajaDeVersion.innerHTML = await obtenerVersion();

f81dfdb4b4858ee166c1a46c2daeaaf200ed1668
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-08-18 15:48:24 -0300

    refactor: reorganizada estructura de /frontend

---
+++ frontend/server.js
@@ -0,0 +1,20 @@
+import { serve } from "bun";
+import path from "path";
+
+const server = serve({
+  port: 5173,
+  async fetch(req) {
+    const url = new URL(req.url);
+
+    // Default -> servir index.html
+    let filePath = url.pathname === "/" ? "/index.html" : url.pathname;
+
+    try {
+      return new Response(Bun.file(`public${filePath}`));
+    } catch {
+      return new Response("Archivo no encontrado", { status: 404 });
+    }
+  },
+});
+
+console.log(`🚀 Servidor corriendo en http://localhost:${server.port}`);

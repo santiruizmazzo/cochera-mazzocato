afc473a745d224824552bac402fe8b290a8a417f
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-09-26 11:31:09 -0300

    build(front): mejorado script de creacion de sprite svg de iconos

--- frontend/create.sh
+++ frontend/create.sh
@@ -7,5 +7,7 @@ OUTPUT=$1.svg
 
 echo '<?xml version="1.0" encoding="utf-8"?>' >$OUTPUT
 echo '<svg xmlns="http://www.w3.org/2000/svg">' >>$OUTPUT
+echo '    <defs>' >>$OUTPUT
 sed s/svg/symbol/g $INPUT/*.svg >>$OUTPUT
+echo '    </defs>' >>$OUTPUT
 echo '</svg>' >>$OUTPUT

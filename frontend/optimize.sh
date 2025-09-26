98cbf753a11e1ce795aabead28fc0b838290c51f
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-09-26 12:59:14 -0300

    build(front): ahora todo icono de /icons se optimiza y sobreescribe el original raw

--- frontend/optimize.sh
+++ frontend/optimize.sh
@@ -8,8 +8,7 @@ if [ -z "$1" ]; then
     INPUT="public/assets/icons"
 fi
 
-OUTPUT="${INPUT}_optimized"
+OUTPUT=$INPUT
 
-echo "Optimizando: $INPUT -> $OUTPUT"
 bun svgo -f $INPUT -o $OUTPUT --config=svgo.config.js --multipass --pretty -p 2
 

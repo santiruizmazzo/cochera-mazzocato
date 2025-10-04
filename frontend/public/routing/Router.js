0b96cd9fef9412fb93507ed98a088defdd3bd897
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-10-04 00:14:53 -0300

    feat(front): ahora la app es capaz de manejar url params cuando hace el routing + creada view especifica para la info de un inquilino

--- frontend/public/routing/Router.js
+++ frontend/public/routing/Router.js
@@ -1,13 +1,28 @@
 import HomeView from "../views/HomeView.js";
 import PaymentsView from "../views/PaymentsView.js";
 import TenantsView from "../views/TenantsView.js";
+import TenantDetailView from "../views/TenantDetailView.js";
 
 export default class Router {
   routes = [
     { path: "/", view: HomeView },
     { path: "/pagos", view: PaymentsView },
     { path: "/inquilinos", view: TenantsView },
+    { path: "/inquilinos/:id", view: TenantDetailView },
   ];
+
+  pathToRegex(path) {
+    return new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+  }
+
+  getParams(match) {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+      return [key, values[i]];
+    }));
+  }
 
   navigateTo(url) {
     if (url.endsWith(location.pathname)) return;
@@ -20,23 +35,22 @@ export default class Router {
     const potentialMatches = this.routes.map((route) => {
       return {
         route: route,
-        isMatch: location.pathname === route.path,
+        result: location.pathname.match(this.pathToRegex(route.path)),
       };
     });
 
     let match = potentialMatches.find(
-      (potentialMatch) => potentialMatch.isMatch,
+      (potentialMatch) => potentialMatch.result !== null,
     );
 
     if (!match) {
       match = {
         route: this.routes[0],
-        isMatch: true,
+        result: [location.pathname],
       };
     }
 
-    console.log(match);
-    const view = new match.route.view();
+    const view = new match.route.view(this.getParams(match));
 
     document.querySelector("main").innerHTML = await view.getHtml();
     view.setUpJavascript();
--- frontend/public/views/TenantDetailView.js
+++ frontend/public/views/TenantDetailView.js
@@ -7,8 +7,8 @@ export default class extends AbstractView {
 
   async getHtml() {
     return /*html*/ `
-      <section class="payments-view">
-        <h2>Acá se encontrarán todos los pagos de cuotas recibidos... (próximamente)</h2>
+      <section class="tenant-detail-view">
+        <h2>Aca deberia mostrarte la info del inquilino con id = ${this.params.id}</h2>
       </section>
     `;
   }

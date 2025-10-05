814a7dfd26ddd289728850993026cdac99496fa5
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-10-04 23:20:08 -0300

    refactor(front): formateado codigo de router

--- frontend/public/routing/Router.js
+++ frontend/public/routing/Router.js
@@ -12,16 +12,22 @@ export default class Router {
   ];
 
   pathToRegex(path) {
-    return new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+    return new RegExp(
+      "^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$",
+    );
   }
 
   getParams(match) {
     const values = match.result.slice(1);
-    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+      (result) => result[1],
+    );
 
-    return Object.fromEntries(keys.map((key, i) => {
-      return [key, values[i]];
-    }));
+    return Object.fromEntries(
+      keys.map((key, i) => {
+        return [key, values[i]];
+      }),
+    );
   }
 
   navigateTo(url) {

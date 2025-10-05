f0d4c78288f691a7cf0fb37d3f6903cbcba46de6
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-10-05 11:52:17 -0300

    style(front): mejora preliminar de estilos del detalle de inquilino

--- frontend/public/components/TenantFullCard.js
+++ frontend/public/components/TenantFullCard.js
@@ -1,21 +1,47 @@
 const template = document.createElement("template");
 template.innerHTML = /*html*/ `
   <style>
-    div {
-      min-width: 25dvw;
-      min-height: 7rem;
+    :host {
+      height: 100%;
+      min-width: 65%;
+      display: flex;
+    }
+
+    :host > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
       background-color: var(--clr-main-darker);
+    }
+    
+    h2, p {
+      margin: 0;
+    }
+
+    h2 {
+      text-align: center;
+      font-size: 3.5rem;
+      padding: 1rem 0;
+    }
+
+    #info {
+      flex-grow: 1;
       padding: 1rem;
+    }
+
+    #entry-month {
+      text-align: center;
+      background-color: var(--clr-main-darkest);
+      padding: 0.4rem 0;
+    }
+
+    .empty {
+      align-items: center;
+      justify-content: center;
     }
   </style>
 
-  <div>
-    <h2 id="full-name"></h2>
-    <p id="dni"></p>
-    <p id="address"></p>
-    <p id="phone"></p>
-    <p id="email"></p>
-    <p id="entry-month"></p>
+  <div class="empty">
     <slot></slot>
   </div>
 `;
@@ -47,20 +73,46 @@ export default class TenantFullCard extends HTMLElement {
   }
 
   render() {
-    this.shadowRoot.querySelector("slot").style.display = "none";
-    this.shadowRoot.querySelector("#full-name").innerHTML =
-      `${this.tenantData["name"]} ${this.tenantData["last_name"]}`;
-    this.shadowRoot.querySelector("#dni").innerHTML =
-      `DNI ${this.tenantData["dni"]}`;
-    this.shadowRoot.querySelector("#address").innerHTML =
-      `Domicilio: ${this.tenantData["address"]}`;
-    this.shadowRoot.querySelector("#phone").innerHTML =
-      `Teléfono: ${this.tenantData["phone"]}`;
-    this.shadowRoot.querySelector("#email").innerHTML =
-      `Email: ${this.tenantData["email"]}`;
-    this.shadowRoot.querySelector("#entry-month").innerHTML =
-      `Mes de ingreso: ${this.tenantData["entry_month"]}`;
+    const container = this.shadowRoot.querySelector("div");
+    container.innerHTML = "";
+    container.className = "loaded";
+
+    const fullNameElement = document.createElement("h2");
+    fullNameElement.innerHTML = `${this.tenantData["name"]} ${this.tenantData["last_name"]}`;
+    container.appendChild(fullNameElement);
+
+    const infoContainer = document.createElement("div");
+    infoContainer.id = "info";
+
+    const dniElement = document.createElement("p");
+    dniElement.innerHTML = `DNI: ${this.tenantData["dni"]}`;
+    infoContainer.appendChild(dniElement);
+
+    const addressElement = document.createElement("p");
+    addressElement.innerHTML = `Domicilio: ${this.tenantData["address"] || "❓"}`;
+    infoContainer.appendChild(addressElement);
+
+    const phoneElement = document.createElement("p");
+    phoneElement.innerHTML = `Teléfono: ${this.tenantData["phone"] || "❓"}`;
+    infoContainer.appendChild(phoneElement);
+
+    const emailElement = document.createElement("p");
+    emailElement.innerHTML = `Email: ${this.tenantData["email"] || "❓"}`;
+    infoContainer.appendChild(emailElement);
+
+    container.appendChild(infoContainer);
+
+    const entryMonthElement = document.createElement("p");
+    entryMonthElement.id = "entry-month";
+    entryMonthElement.innerHTML = `Ingresó en ${formatDateMMYYYY(this.tenantData["entry_month"])}`;
+    container.appendChild(entryMonthElement);
   }
+}
+
+function formatDateMMYYYY(input) {
+  const [month, year] = input.split("-").map(Number);
+  const date = new Date(year, month - 1);
+  return date.toLocaleDateString("es-ES", { month: "long", year: "numeric" });
 }
 
 customElements.define("tenant-full-card", TenantFullCard);

16bc3666e7fce8d80c36b7664429dedc0a53d8b5
Santiago Ruiz Mazzocato <santiago@email.com>
2025-10-06 17:59:01 +0000

    test(back): testeado cuando se instancia un DNI desde un float

--- backend/domain/dni.go
+++ backend/domain/dni.go
@@ -1,9 +1,16 @@
 package domain
 
 type DNI struct {
-	value uint32
+	Value uint32
 }
 
-func NewDNI(number uint32) (DNI, error) {
-	return DNI{value: number}, nil
+func NewDNI(number any) (DNI, error) {
+	switch value := number.(type) {
+	case float64:
+		return DNI{Value: uint32(value)}, nil
+	case int:
+		return DNI{Value: uint32(value)}, nil
+	default:
+		return DNI{}, nil
+	}
 }
--- backend/tests/unit/dni_test.go
+++ backend/tests/unit/dni_test.go
@@ -12,3 +12,15 @@ func TestCreateValidDNI(t *testing.T) {
 		t.Fatalf("DNI no debe devolver error cuando es un número válido")
 	}
 }
+
+func TestCreateValidDNIFromFloatNumber(t *testing.T) {
+	dni, err := domain.NewDNI(12345678.8298)
+
+	if err != nil {
+		t.Fatalf("DNI no debe devolver error cuando es un número válido")
+	}
+
+	if dni.Value != 12345678 {
+		t.Fatalf("El valor del DNI debería igual a la parte entera del float")
+	}
+}

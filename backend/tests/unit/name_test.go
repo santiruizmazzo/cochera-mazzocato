fd0a27c87f0e52b97f10feeba4ed1247e494b980
Santiago Ruiz Mazzocato <santiago@email.com>
2025-10-07 01:39:14 +0000

    test(back): agregado test para caso fallido de decodificado de Name

--- backend/tests/unit/name_test.go
+++ backend/tests/unit/name_test.go
@@ -42,3 +42,16 @@ func TestCreateValidNameFromJSON(t *testing.T) {
 		t.Fatalf("Name decodificado de string desde json no coincide")
 	}
 }
+
+func TestFailToCreateNameFromJSON(t *testing.T) {
+	name := domain.Name{}
+	err := name.UnmarshalJSON([]byte(`{"id":2}`))
+
+	if err == nil {
+		t.Fatalf("Name debe devolver error cuando se decodifica valor no string desde json")
+	}
+
+	if name.Value != "" {
+		t.Fatalf("El valor del Name no decodificado correctamente debe ser vacío")
+	}
+}

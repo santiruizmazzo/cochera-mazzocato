10615d12e9d4c64266fbdbf03900d0849b7efe5d
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-10-13 21:02:53 -0300

    style(front): creada estructura y estilos preliminares para mapa de status de plazas en la vista de inicio de la app

--- frontend/public/views/HomeView.js
+++ frontend/public/views/HomeView.js
@@ -9,6 +9,56 @@ export default class extends AbstractView {
     return /*html*/ `
       <section class="home-view">
         <h2>¡Bienvenido a la app de gestión de la <span>Cochera Mazzocato</span>!</h2>
+        <div class="slots-collection">
+          <div class="slot">
+            <h2>1</h2>
+            <p>Alquilada por: <a href="/inquilinos/3" data-link>Ruiz Mazzocato</a></p>
+          </div>
+          <div class="slot">
+            <h2>2</h2>
+            <p>Alquilada por: <a href="/inquilinos/6" data-link>Lamponne</a></p>
+          </div>
+          <div class="slot">
+            <h2>3</h2>
+            <p>Alquilada por: <a href="/inquilinos/8" data-link>Ravenna</a></p>
+          </div>
+          <div class="slot">
+            <h2>4</h2>
+            <p>Alquilada por: <a href="/inquilinos/6" data-link>Lamponne</a></p>
+          </div>
+          <div class="slot">
+            <h2>5</h2>
+            <p>Alquilada por: <a href="/inquilinos/9" data-link>Santos</a></p>
+          </div>
+          <div class="slot">
+            <h2>6</h2>
+            <p>Alquilada por: <a href="/inquilinos/9" data-link>Santos</a></p>
+          </div>
+          <div class="slot">
+            <h2>7</h2>
+            <p>Alquilada por: <a href="/inquilinos/13" data-link>Hamlin</a></p>
+          </div>
+          <div class="slot">
+            <h2>8</h2>
+            <p>Alquilada por: <a href="/inquilinos/14" data-link>Goodman</a></p>
+          </div>
+          <div class="slot">
+            <h2>9</h2>
+            <p>Alquilada por: <a href="/inquilinos/10" data-link>Medina</a></p>
+          </div>
+          <div class="slot">
+            <h2>10</h2>
+            <p>Libre 😧</p>
+          </div>
+          <div class="slot">
+            <h2>11</h2>
+            <p>Libre 😧</p>
+          </div>
+          <div class="slot">
+            <h2>12</h2>
+            <p>Alquilada por: <a href="/inquilinos/14" data-link>Goodman</a></p>
+          </div>
+        </div>
       </section>
     `;
   }

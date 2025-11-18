96c16461ab82e159f4e1708608dc2f0e7313a397
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-11-17 23:02:07 -0300

    feat(front): creada version mockup de asignacion de inquilino a plaza

--- frontend/public/views/HomeView.js
+++ frontend/public/views/HomeView.js
@@ -9,57 +9,108 @@ export default class extends AbstractView {
     return /*html*/ `
       <section class="home-view">
         <h2>¡Bienvenido a la app de gestión de la <span>Cochera Mazzocato</span>!</h2>
+        
+        <dialog id="assign-tenant-to-slot-modal" closedby="any">
+          <header>
+            <h3>Asignar inquilino a plaza</h3>
+            <button class="close-action-btn close-modal-btn">
+              <svg><use href="#close"/></svg>
+            </button>
+          </header>
+
+          <form>
+            <select name="">
+              <option value="1">Pablo Lamponne</option>
+              <option value="2">Emilio Ravenna</option>
+            </select>
+            <div>
+              <activatable-button type="submit">Confirmar</activatable-button>
+              <error-box></error-box>
+            </div>
+          </form>
+        </dialog>
+
         <div class="slots-collection">
-          <div class="slot">
+          <div id="1" class="slot">
             <h2>1</h2>
-            <p>Alquilada por: <a href="/inquilinos/3" data-link>Ruiz Mazzocato</a></p>
+            <p>De <a href="/inquilinos/1" data-link>Lamponne</a></p>
           </div>
-          <div class="slot">
+          <div id="2" class="slot">
             <h2>2</h2>
-            <p>Alquilada por: <a href="/inquilinos/6" data-link>Lamponne</a></p>
+            <p>De <a href="/inquilinos/1" data-link>Lamponne</a></p>
           </div>
-          <div class="slot">
+          <div id="3" class="slot">
             <h2>3</h2>
-            <p>Alquilada por: <a href="/inquilinos/8" data-link>Ravenna</a></p>
+            <p>De <a href="/inquilinos/1" data-link>Lamponne</a></p>
           </div>
-          <div class="slot">
+          <div id="4" class="slot">
             <h2>4</h2>
-            <p>Alquilada por: <a href="/inquilinos/6" data-link>Lamponne</a></p>
+            <p>De <a href="/inquilinos/1" data-link>Lamponne</a></p>
           </div>
-          <div class="slot">
+          <div id="5" class="slot">
             <h2>5</h2>
-            <p>Alquilada por: <a href="/inquilinos/9" data-link>Santos</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
-          <div class="slot">
+          <div id="6" class="slot">
             <h2>6</h2>
-            <p>Alquilada por: <a href="/inquilinos/9" data-link>Santos</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
-          <div class="slot">
+          <div id="7" class="slot">
             <h2>7</h2>
-            <p>Alquilada por: <a href="/inquilinos/13" data-link>Hamlin</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
-          <div class="slot">
+          <div id="8" class="slot">
             <h2>8</h2>
-            <p>Alquilada por: <a href="/inquilinos/14" data-link>Goodman</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
-          <div class="slot">
+          <div id="9" class="slot">
             <h2>9</h2>
-            <p>Alquilada por: <a href="/inquilinos/10" data-link>Medina</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
-          <div class="slot">
+          <div id="10" class="slot">
             <h2>10</h2>
-            <p>Libre 😧</p>
+            <div class="assign-tenant-btn open-modal-btn">
+              Asignar
+              <svg><use href="#person_waving" /></svg>
+            </div>
           </div>
-          <div class="slot">
+          <div id="11" class="slot">
             <h2>11</h2>
-            <p>Libre 😧</p>
+            <div class="assign-tenant-btn open-modal-btn">
+              Asignar
+              <svg><use href="#person_waving" /></svg>
+            </div>
           </div>
-          <div class="slot">
+          <div id="12" class="slot">
             <h2>12</h2>
-            <p>Alquilada por: <a href="/inquilinos/14" data-link>Goodman</a></p>
+            <p>De <a href="/inquilinos/2" data-link>Ravenna</a></p>
           </div>
         </div>
+        <div></div>
       </section>
     `;
+  }
+
+  setUpJavascript() {
+    const modal = document.querySelector("#assign-tenant-to-slot-modal");
+    const openModalButtons = document.querySelectorAll(".open-modal-btn");
+    const closeModal = document.querySelector(".close-modal-btn");
+    // const form = document.querySelector("tenant-form");
+
+    openModalButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const modalTitle = document.querySelector(
+          "#assign-tenant-to-slot-modal h3",
+        );
+        modalTitle.innerHTML = `Asignar inquilino a plaza N° ${button.parentElement.id}`;
+        modal.showModal();
+      });
+    });
+    // openModal.addEventListener("click", () => modal.showModal());
+    closeModal.addEventListener("click", () => {
+      modal.close();
+      form.clear();
+    });
+    // modal.addEventListener("tenants:update", () => modal.close());
   }
 }

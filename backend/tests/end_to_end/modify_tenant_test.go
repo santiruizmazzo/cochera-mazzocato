751eada93ed43a3f73fef3471e0f2684ef318120
Santiago Ruiz Mazzocato <santiago@email.com>
2026-01-02 18:31:18 +0000

    test(back): creado test para verificar que la modificacion parcial de un inquilino no convierte en nulos los atributos no enviados por el body

--- backend/tests/end_to_end/modify_tenant_test.go
+++ backend/tests/end_to_end/modify_tenant_test.go
@@ -162,3 +162,54 @@ func TestModifyTenantFailsWhenDeletingRequiredAttribute_EndToEnd(t *testing.T) {
 
 	utils.AssertResponseContains(responseMap, "detail", "el DNI debe ser un número entero", t)
 }
+
+func TestModifyTenantDoesNotNullifyValueOfMissingAttributeInRequestBody_EndToEnd(t *testing.T) {
+	t.Skip()
+	if testing.Short() {
+		t.Skip()
+	}
+
+	testAPI.ClearTenants()
+
+	expectedTenant := map[string]any{
+		"dni":         14571272,
+		"name":        "Juan Alberto",
+		"last_name":   "García",
+		"entry_month": "01-2025",
+		"email":       "juanalberto@garcia.com",
+		"address":     "Calle Larios 123",
+		"phone":       "+5494444192929",
+	}
+
+	response, err := testAPI.CreateTenant(expectedTenant)
+	if err != nil {
+		t.Fatal("Failed creating tenant: ", err)
+	}
+
+	modifiedTenant := map[string]any{
+		"dni": 55,
+	}
+
+	response, err = testAPI.ModifyTenant(1, modifiedTenant)
+	if err != nil {
+		t.Fatalf("Failed sending PATCH request to %s: %v", testAPI.GetTenantsRoute(), err)
+	}
+
+	defer func() {
+		if cerr := response.Body.Close(); cerr != nil {
+			t.Fatalf("Failed closing response body: %v", cerr)
+		}
+	}()
+
+	responseMap := utils.CreateMapFromBody(response.Body, t)
+
+	utils.AssertStatusCodeIs(http.StatusOK, response.StatusCode, t)
+
+	utils.AssertResponseContains(responseMap, "dni", float64(55), t)
+	utils.AssertResponseContains(responseMap, "name", "Juan Alberto", t)
+	utils.AssertResponseContains(responseMap, "last_name", "García", t)
+	utils.AssertResponseContains(responseMap, "entry_month", "01-2025", t)
+	utils.AssertResponseContains(responseMap, "email", "juanalberto@garcia.com", t)
+	utils.AssertResponseContains(responseMap, "address", "Calle Larios 123", t)
+	utils.AssertResponseContains(responseMap, "phone", "+5494444192929", t)
+}

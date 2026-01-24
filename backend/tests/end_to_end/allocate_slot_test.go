e9b013b21a1ae5500ee53d791f7e245377bf5830
Santiago Ruiz Mazzocato <santiago@email.com>
2026-01-24 19:37:34 +0000

    test(back): ajustado test e2e para caso de asignacion de plaza a inquilino

--- backend/tests/end_to_end/allocate_slot_test.go
+++ backend/tests/end_to_end/allocate_slot_test.go
@@ -9,20 +9,18 @@ import (
 )
 
 func TestAllocateFreeSlotToTenant_EndToEnd(t *testing.T) {
-	t.Skip("SKIPEANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-
+	t.Skip()
 	if testing.Short() {
 		t.Skip()
 	}
 
 	jsonBody, _ := json.Marshal(map[string]int{
 		"tenant_id": 1,
-		"slot_id":   1,
 	})
 
-	response, err := utils.HTTPPut(testAPI.GetSlotsRoute(), "application/json", bytes.NewBuffer(jsonBody))
+	response, err := utils.HTTPPatch(testAPI.GetSlotsRoute()+"/1", "application/json", bytes.NewBuffer(jsonBody))
 	if err != nil {
-		t.Fatalf("Failed sending PUT request to %s: %v", testAPI.GetSlotsRoute(), err)
+		t.Fatalf("Failed sending PATCH request to %s: %v", testAPI.GetSlotsRoute(), err)
 	}
 
 	defer func() {
@@ -35,7 +33,7 @@ func TestAllocateFreeSlotToTenant_EndToEnd(t *testing.T) {
 
 	utils.AssertStatusCodeIs(http.StatusOK, response.StatusCode, t)
 
-	utils.AssertResponseContains(responseMap, "slot_id", 1, t)
-
+	utils.AssertResponseContains(responseMap, "id", 1, t)
+	utils.AssertResponseContains(responseMap, "number", 1, t)
 	utils.AssertResponseContains(responseMap, "tenant_id", 1, t)
 }

5653264b5597c566fc4acd370fe372c906cc307a
Santiago Ruiz Mazzocato <santiago@email.com>
2026-01-26 18:50:29 +0000

    test(back): creado test de integracion para caso feliz de obtener plaza por id

--- backend/tests/integration/postgres_slot_repository_test.go
+++ backend/tests/integration/postgres_slot_repository_test.go
@@ -30,3 +30,31 @@ func TestPostgresSlotRepository_Save_Successfully_Integration(t *testing.T) {
 		t.Fatal("Expected slot is different from saved slot")
 	}
 }
+
+func TestPostgresSlotRepository_GetByID_Successfully_Integration(t *testing.T) {
+	t.Skip()
+	if testing.Short() {
+		t.Skip()
+	}
+
+	tenantsRepo := infra.NewPostgresTenantRepository(db)
+
+	localTenant, _ := ent.NewTenant(1, 123, "Santi", "Ruiz", "Rosales 54", "+543333111111", "santiruiz@live.com", "12-2022")
+
+	_, _ = tenantsRepo.Save(localTenant)
+
+	slotsRepo := infra.NewPostgresSlotRepository(db)
+
+	localSlot, _ := ent.NewSlot(6, 6, 1)
+
+	_, _ = slotsRepo.Save(localSlot)
+
+	savedSlot, err := slotsRepo.GetByID(6)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *localSlot != *savedSlot {
+		t.Fatal("Expected slot is different from saved slot")
+	}
+}
